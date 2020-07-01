09ea6647003326a4c116859bd19795e762a163e5
Faschz <faschz@gmail.com>
2020-07-01 18:43:36 -0500

    Decomp 12 functions in unknown_001050
    func_80000B18, func_80000B28, func_80000C1C, func_80000C2C, func_80000CBC, func_80001050, func_800015B8, func_800015C8, func_800018E0, func_80001980, func_80001AEC, and musicVolumeSliderPercentage

--- src/unknown_001050.c
+++ src/unknown_001050.c
@@ -4,21 +4,63 @@
 #include "types.h"
 #include "macros.h"
 
+extern u8 D_800DC638;
+extern s8 D_800DC670;
+extern u8 D_80115D05;
+
+extern s16 D_80115D30;
+
+extern s32 D_800DC634;
+extern s32 D_800DC648;
+extern s32 D_800DC654;
+extern s32 D_800DC658;
+extern s32 D_80115D38;
+extern s32 D_80115D3C;
+
+extern s32* D_800DC630;
+
+extern f32 D_800DC650;
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000450.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000890.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000968.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000B18.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000B28.s")
+
+void func_80000B18(void) {
+    D_800DC648 = 1;
+}
+
+void func_80000B28(void) {
+    D_800DC648 = 0;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000B34.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000BE0.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000C1C.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000C2C.s")
+
+void func_80000C1C(void) {
+    D_800DC670 = 1;
+}
+
+void func_80000C2C(void) {
+    D_800DC670 = 0;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000C38.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000C98.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000CBC.s")
+
+void func_80000CBC(void) {
+    D_80115D38 = 0;
+    D_80115D3C = 0;
+    D_800DC650 = 1.0f;
+    func_80001990(D_800DC638);
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000D00.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80000FDC.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001050.s")
+
+void func_80001050(void) {
+    D_800DC658 = 0;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_8000105C.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001074.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001114.s")
@@ -31,19 +73,44 @@ GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800012A8.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800012E8.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800014BC.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001534.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800015B8.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800015C8.s")
+
+s16 func_800015B8(void) {
+    return D_80115D30;
+}
+
+s32 func_800015C8(void) {
+    return (func_800C7A50(D_800DC630) == 1) & 0xFF;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800015F8.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001784.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800017D4.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001844.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_800018E0.s")
+
+void func_800018E0(void) {
+    if (func_80001C08() == 0) {
+        D_80115D05 = 0;
+        func_80002570(D_800DC634);
+    }
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001918.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001980.s")
+
+u8 func_80001980(void) {
+    return D_80115D05;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001990.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001A3C.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001AEC.s")
-GLOBAL_ASM("asm/non_matchings/unknown_001050/musicVolumeSliderPercentage.s")
+
+u8 func_80001AEC(void) {
+    return D_800DC638;
+}
+
+s32 musicVolumeSliderPercentage(void) {
+    return D_800DC654;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001B0C.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001B58.s")
 GLOBAL_ASM("asm/non_matchings/unknown_001050/func_80001BC0.s")
