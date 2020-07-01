4e0e107b163f8640cf55c295c19ebadbfcb25149
Faschz <faschz@gmail.com>
2020-07-01 13:35:40 -0500

    Decomp 7 functions in unknown_043920
    func_80044450, func_80052188, func_800599A8, func_80059B4C, func_8005A3B0, func_8005A3C0, and func_8005C25C

--- asm/non_matchings/unknown_043920/func_8005A3B0.s
+++
@@ -1,6 +0,0 @@
-glabel func_8005A3B0
-/* 05AFB0 8005A3B0 240E0001 */  li    $t6, 1
-/* 05AFB4 8005A3B4 3C018012 */  lui   $at, 0x8012
-/* 05AFB8 8005A3B8 03E00008 */  jr    $ra
-/* 05AFBC 8005A3BC A02ED584 */   sb    $t6, -0x2a7c($at)
-
