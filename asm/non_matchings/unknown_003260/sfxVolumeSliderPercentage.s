b244c57866cc3b5737a233ba06609bf3923cb7a4
David Benepe <benepe96@gmail.com>
2020-06-30 19:50:49 -0500

    split unknown_003260

---
+++ asm/non_matchings/unknown_003260/sfxVolumeSliderPercentage.s
@@ -0,0 +1,6 @@
+glabel sfxVolumeSliderPercentage
+/* 003D7C 8000317C 3C02800E */  lui   $v0, %hi(D_800DC6C0) # $v0, 0x800e
+/* 003D80 80003180 8C42C6C0 */  lw    $v0, %lo(D_800DC6C0)($v0)
+/* 003D84 80003184 03E00008 */  jr    $ra
+/* 003D88 80003188 00000000 */   nop   
+
---
+++ src/unknown_003260.c
@@ -0,0 +1,35 @@
+/* The comment below is needed for this file to be picked up by generate_ld */
+/* RAM_POS: 0x80002660 */
+
+#include "types.h"
+#include "macros.h"
+
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80002660.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80002A50.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80002A74.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/D_80002A98.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80002C00.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80002DF8.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/D_80002E38.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/D_80003008.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80003040.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80003160.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/sfxVolumeSliderPercentage.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000318C.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800031C0.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/D_800033C8.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80003470.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000410C.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000418C.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800041FC.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800042CC.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004384.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004520.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004604.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004638.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004668.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000488C.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800048D8.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800049D8.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800049F8.s")
+GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004A60.s")
