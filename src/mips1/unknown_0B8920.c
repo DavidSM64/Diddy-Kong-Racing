f1698ca5553076fac36b5a568558e41d691b4c32
David Benepe <benepe96@gmail.com>
2020-07-03 21:52:45 -0500

    Decompiled 4 functions in unknown_0B8920.c

--- src/mips1/unknown_0B8920.c
+++ src/mips1/unknown_0B8920.c
@@ -4,7 +4,80 @@
 #include "types.h"
 #include "macros.h"
 
-GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800B7D20.s")
+extern s32 D_800E3760;
+extern s32 D_800E3764;
+extern s32 D_800E3768;
+extern s32 D_800E376C;
+extern s32 D_8012AAD8;
+extern s32 D_8012AAD4;
+extern s32 D_8012AAD0;
+
+s32  func_80070C9C(s32 arg0, s32 arg1);
+void func_80071140(s32* arg0);
+void func_8007B2BC(s32* arg0);
+
+extern s32* D_800E3040;
+extern s32* D_800E3044;
+extern s32* D_800E3048;
+extern s32* D_800E304C;
+extern s32* D_800E3070;
+extern s32* D_800E3080;
+extern s32* D_800E30D0;
+extern s32* D_800E30D4;
+extern s32* D_800E30D8;
+extern s32* D_800E3178;
+extern s32* D_800E3184;
+extern s32* D_800E3188;
+extern s32* D_800E3190;
+extern s32* D_800E3194;
+
+void func_800B7D20(void) {
+    if (D_800E3040 != NULL) {
+        func_80071140(D_800E3040);
+        D_800E3040 = NULL;
+    }
+    if (D_800E3044 != NULL) {
+        func_80071140(D_800E3044);
+        D_800E3044 = NULL;
+    }
+    if (D_800E3048 != NULL) {
+        func_80071140(D_800E3048);
+        D_800E3048 = NULL;
+    }
+    if (D_800E304C != NULL) {
+        func_80071140(D_800E304C);
+        D_800E304C = NULL;
+    }
+    if (D_800E3070 != NULL) {
+        func_80071140(D_800E3070);
+        D_800E3070 = NULL;
+    }
+    if (D_800E3080 != NULL) {
+        func_80071140(D_800E3080);
+        D_800E3080 = NULL;
+    }
+    if (D_800E30D0 != NULL) {
+        func_8007B2BC(D_800E30D0);
+        D_800E30D0 = NULL;
+    }
+    if (D_800E30D4 != NULL) {
+        func_80071140(D_800E30D4);
+        D_800E30D4 = NULL;
+    }
+    if (D_800E30D8 != NULL) {
+        func_80071140(D_800E30D8);
+        D_800E30D8 = NULL;
+    }
+    if (D_800E3178 != NULL) {
+        func_80071140(D_800E3178);
+        D_800E3178 = NULL;
+    }
+    D_800E3190 = NULL;
+    D_800E3194 = NULL;
+    D_800E3184 = NULL;
+    D_800E3188 = NULL;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800B7EB4.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800B8134.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800B82B4.s")
@@ -96,13 +169,41 @@ GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C5AA0.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C5B58.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C5F60.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6000.s")
-GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6170.s")
-GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C61AC.s")
+
+void func_800C6170(void) {
+    D_800E3760 = func_80070C9C(0x2800, 0xFF);
+    D_800E3764 = func_80070C9C(0x10, 0xFF);
+}
+
+// Returns the little-endian value from a byte array.
+s32 byteswap32(u8* arg0) {
+    s32 value;
+    value = *arg0++;
+    value |= (*arg0++ << 8);
+    value |= (*arg0++ << 16);
+    value |= (*arg0++ << 24);
+    return value;
+}
+
+
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C61DC.s")
-GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6218.s")
+
+s32 func_800C68C0(void);
+
+s32 func_800C6218(s32 arg0, s32 arg1) {
+    // The compression header is 5 bytes. Maybe this is part of the decompression routine?
+    D_800E3768 = (s32) (arg0 + 5); 
+    D_800E376C = arg1;
+    D_8012AAD4 = 0;
+    D_8012AAD0 = 0;
+    while(func_800C68C0() != 0) {} // Keep calling func_800C68C0() until it returns 0.
+    return arg1;
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6274.s")
+
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C68C0.s")
-GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C69AC.s")
+
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C69C4.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6B90.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0B8920/func_800C6DDC.s")
