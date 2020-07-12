11caf007ab3b6e9cf62bd3e546c5a3ee6ba17e65
Faschz <faschz@gmail.com>
2020-07-12 12:26:54 -0500

    Decomp 34 functions in unknown_066AA0
    func_8006B224, func_8006B240, func_8006BD88, func_8006BDB0, func_8006BDC0, func_8006C164, func_8006C18C, func_8006C19C, func_8006C2E4, func_8006C2F0, func_8006D8A4, func_8006D8E0, func_8006DA0C, func_8006DA1C, func_8006DB14, func_8006DB20, func_8006DB2C, func_8006EA90, func_8006EAA0, func_8006EAB0, func_8006EB14, func_8006EB24, func_8006EB40, func_8006EB5C, func_8006EBA8, func_8006EBC4, func_8006EBE0, func_8006EBFC, func_8006ECE0, func_8006F254, func_8006F338, func_8006F388, func_8006F42C, and func_8006F4C8

--- asm/non_matchings/unknown_066AA0/func_8006EBA8.s
+++
@@ -1,9 +0,0 @@
-glabel func_8006EBA8
-/* 06F7A8 8006EBA8 3C02800E */  lui   $v0, %hi(D_800DD37C) # $v0, 0x800e
-/* 06F7AC 8006EBAC 2442D37C */  addiu $v0, %lo(D_800DD37C) # addiu $v0, $v0, -0x2c84
-/* 06F7B0 8006EBB0 8C4E0000 */  lw    $t6, ($v0)
-/* 06F7B4 8006EBB4 00000000 */  nop   
-/* 06F7B8 8006EBB8 35CF0008 */  ori   $t7, $t6, 8
-/* 06F7BC 8006EBBC 03E00008 */  jr    $ra
-/* 06F7C0 8006EBC0 AC4F0000 */   sw    $t7, ($v0)
-
--- asm/non_matchings/unknown_066AA0/func_8006EBE0.s
+++
@@ -1,9 +0,0 @@
-glabel func_8006EBE0
-/* 06F7E0 8006EBE0 3C02800E */  lui   $v0, %hi(D_800DD37C) # $v0, 0x800e
-/* 06F7E4 8006EBE4 2442D37C */  addiu $v0, %lo(D_800DD37C) # addiu $v0, $v0, -0x2c84
-/* 06F7E8 8006EBE8 8C4E0000 */  lw    $t6, ($v0)
-/* 06F7EC 8006EBEC 00000000 */  nop   
-/* 06F7F0 8006EBF0 35CF0020 */  ori   $t7, $t6, 0x20
-/* 06F7F4 8006EBF4 03E00008 */  jr    $ra
-/* 06F7F8 8006EBF8 AC4F0000 */   sw    $t7, ($v0)
-
