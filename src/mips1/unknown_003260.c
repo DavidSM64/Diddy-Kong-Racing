2f4c8f53a51f1d2a8faf4cba8f80bc03a042954d
Faschz <faschz@gmail.com>
2020-07-26 15:48:26 -0500

    Decomp 2 functions in unknown_003260
    func_8000318C and func_80004638

--- src/mips1/unknown_003260.c
+++ src/mips1/unknown_003260.c
@@ -4,6 +4,14 @@
 #include "types.h"
 #include "macros.h"
 
+/* Unknown Size */
+typedef struct unk800DC6BC {
+    u8  pad00[0x44];
+    s32 unk44;
+    s32 unk48;
+} unk800DC6BC;
+
+extern unk800DC6BC* D_800DC6BC;
 extern s32 D_800DC6C0;
 
 extern s32* D_80115FB0;
@@ -37,7 +45,14 @@ s32 sfxVolumeSliderPercentage(void) {
     return D_800DC6C0;
 }
 
-GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000318C.s")
+void func_8000318C(s32 arg0) {
+    if (D_800DC6BC->unk44 >= arg0) {
+        D_800DC6BC->unk48 = arg0;
+    } else {
+        D_800DC6BC->unk48 = D_800DC6BC->unk44;
+    }
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800031C0.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/D_800033C8.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80003470.s")
@@ -48,7 +63,11 @@ GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800042CC.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004384.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004520.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004604.s")
-GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004638.s")
+
+void func_80004638(s32 arg0, s16 arg1, s32 arg2) {
+    func_80004668(arg0, arg1, 0, arg2);
+}
+
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_80004668.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_8000488C.s")
 GLOBAL_ASM("asm/non_matchings/unknown_003260/func_800048D8.s")
