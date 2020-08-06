d153068805029918b0fec77d61ed77978f5116bb
Mittenshugg <michaelsh12391@gmail.com>
2020-08-06 02:31:00 -0500

    audio documention

--- src/unknown_0C81A0.c
+++ src/unknown_0C81A0.c
@@ -7,6 +7,6 @@
 void dummy_800C75A0(void) {}
 void dummy_800C75A8(void) {}
 
-GLOBAL_ASM("asm/non_matchings/unknown_0C81A0/func_800C75B0.s")
-GLOBAL_ASM("asm/non_matchings/unknown_0C81A0/func_800C76A4.s")
+GLOBAL_ASM("asm/non_matchings/unknown_0C81A0/_bnkfPatchBank.s")
+GLOBAL_ASM("asm/non_matchings/unknown_0C81A0/alBnkfNew.s")
 GLOBAL_ASM("asm/non_matchings/unknown_0C81A0/func_800C77A8.s")
