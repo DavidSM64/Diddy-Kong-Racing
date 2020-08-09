92548427f863c7edb66e5f7e231689c3eb1c931f
David Benepe <benepe96@gmail.com>
2020-08-09 00:21:11 -0500

    (7.28%) Fully decompiled unknown_0C7F50

--- asm/non_matchings/unknown_0C7F50/func_800C73E0.s
+++
@@ -1,6 +0,0 @@
-glabel func_800C73E0
-/* 0C7FE0 800C73E0 3C02800E */  lui   $v0, %hi(D_800E3770) # $v0, 0x800e
-/* 0C7FE4 800C73E4 8C423770 */  lw    $v0, %lo(D_800E3770)($v0)
-/* 0C7FE8 800C73E8 03E00008 */  jr    $ra
-/* 0C7FEC 800C73EC 00000000 */   nop   
-
