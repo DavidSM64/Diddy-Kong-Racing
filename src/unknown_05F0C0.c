de26b50d10b9919e8e9919492351d5671eb7bf20
David Benepe <benepe96@gmail.com>
2021-05-24 21:01:01 -0500

    unknown_05F0C0.c data section

--- src/unknown_05F0C0.c
+++ src/unknown_05F0C0.c
@@ -4,4 +4,15 @@
 #include "types.h"
 #include "macros.h"
 
+/************ .data ************/
+
+s16 D_800DCE40[16] = {
+    0x0244, 0x0242, 0x0243, 0x0245,
+    0x0246, 0x0247, 0x0248, 0x00F6,
+    0x00F7, 0x0225, 0x0226, 0x002E,
+    0x002F, 0x0030, 0x0000, 0x0000
+};
+
+/*******************************/
+
 GLOBAL_ASM("asm/non_matchings/unknown_05F0C0/func_8005E4C0.s")
