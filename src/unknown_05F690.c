9b6e4e30abddb37e4573037b1675f412ec72dc11
David Benepe <benepe96@gmail.com>
2021-05-24 21:06:08 -0500

    unknown_05F690.c data section

--- src/unknown_05F690.c
+++ src/unknown_05F690.c
@@ -4,6 +4,28 @@
 #include "types.h"
 #include "macros.h"
 
+/************ .data ************/
+
+s16 D_800DCE60[16] = {
+    0x022C, 0x00A5, 0x00A6, 0x0057,
+    0x0051, 0x006D, 0x0052, 0x00F8,
+    0x00F9, 0x0227, 0x0228, 0x002E,
+    0x002F, 0x0030, 0x0000, 0x0000
+};
+
+
+s16 D_800DCE80[16] = {
+    0x022C, 0x00A5, 0x00A6, 0x0057,
+    0x0051, 0x006D, 0x0052, 0x00F8,
+    0x00F9, 0x0227, 0x0228, 0x002E,
+    0x002F, 0x0030, 0x0000, 0x0000
+};
+
+s32 D_800DCEA0 = 116315;
+s32 D_800DCEA4 = 1980;
+
+/*******************************/
+
 GLOBAL_ASM("asm/non_matchings/unknown_05F690/func_8005EA90.s")
 GLOBAL_ASM("asm/non_matchings/unknown_05F690/func_8005F310.s")
 GLOBAL_ASM("asm/non_matchings/unknown_05F690/func_8005F850.s")
