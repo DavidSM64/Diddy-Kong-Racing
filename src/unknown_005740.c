4fa3e8e3fb2eca5c06050669580d33e0bb9cad49
David Benepe <benepe96@gmail.com>
2021-05-29 18:29:03 -0500

    Moved .rodata in unknown_005740 from asm to C.

--- src/unknown_005740.c
+++ src/unknown_005740.c
@@ -14,6 +14,68 @@ s32 D_800DC6E0 = 0; // Currently unknown, might be a different type.
 
 /*******************************/
 
+/************ .rodata ************/
+
+const FloatLiteral D_800E4BF0 = { 10000.0f };
+const DoubleLiteral D_800E4BF8 = { 0.4 };
+const FloatLiteral D_800E4C00 = { 10000.0f };
+const DoubleLiteral D_800E4C08 = { 0.05 };
+const DoubleLiteral D_800E4C10 = { 0.95 };
+const DoubleLiteral D_800E4C18 = { 0.13 };
+const DoubleLiteral D_800E4C20 = { 0.3 };
+const DoubleLiteral D_800E4C28 = { 0.02 };
+const DoubleLiteral D_800E4C30 = { 0.01 };
+const FloatLiteral D_800E4C38 = { 0.4f };
+const DoubleLiteral D_800E4C40 = { 0.005 };
+const DoubleLiteral D_800E4C48 = { -0.1 };
+const DoubleLiteral D_800E4C50 = { 0.05 };
+const DoubleLiteral D_800E4C58 = { 0.95 };
+const DoubleLiteral D_800E4C60 = { 0.3 };
+const DoubleLiteral D_800E4C68 = { 6.5534 };
+const FloatLiteral D_800E4C70 = { 6.5534f };
+const FloatLiteral D_800E4C74 = { 10000.0f };
+const FloatLiteral D_800E4C78 = { 10000.0f };
+const DoubleLiteral D_800E4C80 = { 0.05 };
+const DoubleLiteral D_800E4C88 = { 0.95 };
+const DoubleLiteral D_800E4C90 = { 6.5534 };
+const FloatLiteral D_800E4C98 = { 6.5534f };
+const DoubleLiteral D_800E4CA0 = { 1731.23404 };
+const DoubleLiteral D_800E4CA8 = { 0.8 };
+const FloatLiteral D_800E4CB0 = { 0.8f };
+const DoubleLiteral D_800E4CB8 = { -0.8 };
+const FloatLiteral D_800E4CC0 = { -0.8f };
+const FloatLiteral D_800E4CC4 = { 2250000.0f };
+const DoubleLiteral D_800E4CC8 = { 0.05 };
+const FloatLiteral D_800E4CD0 = { 1500.0f };
+const FloatLiteral D_800E4CD4 = { 0.05f };
+const DoubleLiteral D_800E4CD8 = { 0.05 };
+const FloatLiteral D_800E4CE0 = { 2250000.0f };
+const DoubleLiteral D_800E4CE8 = { 0.8 };
+const FloatLiteral D_800E4CF0 = { 1500.0f };
+const FloatLiteral D_800E4CF4 = { 0.05f };
+const DoubleLiteral D_800E4CF8 = { 0.001 };
+
+const char D_800E4D00[] = "OUT OF AUDIO POINTS\n";
+const char D_800E4D18[] = "amAudioLineAddVertex: Exceeded maximum number of lines (%d)\n";
+const char D_800E4D58[] = "amAudioLineAddVertex: Exceeded maximum number of line vertices (%d)\n";
+const char D_800E4DA0[] = "amReverbLineAddVertex: Exceeded maximum number of lines (%d)\n";
+const char D_800E4DE0[] = "amReverbLineAddVertex: Exceeded maximum number of line vertices (%d)\n";
+const char D_800E4E28[] = "Audio line definition error (less than 2 vertices on line %d)\n";
+const char D_800E4E68[] = "Audio line definition error (line=%d, vertex=%d)\n";
+const char D_800E4E9C[] = "Reverb line definition error (less than 2 vertices on line %d)\n";
+const char D_800E4EDC[] = "Reverb line definition error (line=%d, vertex=%d)\n";
+
+const FloatLiteral D_800E4F10 = { -100000.0f };
+const FloatLiteral D_800E4F14 = { 999999.0f };
+const DoubleLiteral D_800E4F18 = { -100000.0 };
+const DoubleLiteral D_800E4F20 = { -100000.0 };
+
+const u32 filePadding[2] = { 0, 0 }; // unknown_005740 needs to be split.
+
+const char D_800E4F30[] = "Exceeded voice limit of %d (%d)\n";
+
+/*******************************/
+
 /************ .bss ************/
 
 extern s8 D_8011AC18;
