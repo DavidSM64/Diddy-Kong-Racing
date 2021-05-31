15d15f53b5c5ee095762f141167a0921ed9960be
Antonio Castelli <aatcastelli@gmail.com>
2021-05-30 23:01:08 -0700

    Moved symbols in unknown_001050 to header file.

---
+++ include/unknown_001050.h
@@ -0,0 +1,25 @@
+#ifndef _UNKNOWN_001050_H_
+#define _UNKNOWN_001050_H_
+
+#include "types.h"
+#include "audio_internal.h"
+
+extern ALCSPlayer* gMusicPlayer;
+extern ALCSPlayer* gSndFxPlayer;
+extern u8 musicRelativeVolume;
+extern u8 sfxRelativeVolume;
+extern u8 D_800DC640;
+extern u8 D_800DC644;
+extern s32 D_800DC648;
+extern s32 audioPrevCount;
+extern f32 D_800DC650;
+extern s32 musicVolumeSliderPercentage;
+extern s32 D_800DC658;
+extern u8 D_800DC65C;
+extern u8  D_800DC660;
+extern s32 D_800DC664;
+extern s32 D_800DC668;
+extern s32 D_800DC66C;
+extern u8 D_800DC670;
+
+#endif
