9df03620a84dc48ef697af54b2d7566a2b491f1d
Antonio Castelli <aatcastelli@gmail.com>
2021-05-30 23:37:09 -0700

    Moved symbols in unknown_005740 to header file.

---
+++ include/unknown_005740.h
@@ -0,0 +1,12 @@
+#ifndef _UNKNOWN_005740_H_
+#define _UNKNOWN_005740_H_
+
+#include "types.h"
+
+extern s8 D_800DC6D0;
+extern s32 D_800DC6D4;
+extern s32 D_800DC6D8;
+extern s32 D_800DC6DC;
+extern s32 D_800DC6E0;
+
+#endif
