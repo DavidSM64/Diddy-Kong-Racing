0194c289244c9d2a04bad055efaf14c2174b4019
Antonio Castelli <aatcastelli@gmail.com>
2021-06-02 19:37:48 -0700

    Moved symbols in unknown_061D30 to header file.

---
+++ include/unknown_061D30.h
@@ -0,0 +1,4 @@
+#ifndef _UNKNOWN_061D30_H_
+#define _UNKNOWN_061D30_H_
+
+#endif
--- src/unknown_061D30.c
+++ src/unknown_061D30.c
@@ -1,6 +1,8 @@
 /* The comment below is needed for this file to be picked up by generate_ld */
 /* RAM_POS: 0x80062290 */
 
+#include "unknown_061D30.h"
+
 #include "types.h"
 #include "macros.h"
 
