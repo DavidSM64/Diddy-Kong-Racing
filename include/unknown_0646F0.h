a50916e56ab3047d99c2d9509f4a31740575d667
Antonio Castelli <aatcastelli@gmail.com>
2021-06-02 19:42:48 -0700

    Moved symbols in unknown_0646F0 to header file.

---
+++ include/unknown_0646F0.h
@@ -0,0 +1,4 @@
+#ifndef _UNKNOWN_0646F0_H_
+#define _UNKNOWN_0646F0_H_
+
+#endif
--- src/unknown_0646F0.c
+++ src/unknown_0646F0.c
@@ -1,6 +1,8 @@
 /* The comment below is needed for this file to be picked up by generate_ld */
 /* RAM_POS: 0x80063AF0 */
 
+#include "unknown_0646F0.h"
+
 #include "types.h"
 #include "macros.h"
 
