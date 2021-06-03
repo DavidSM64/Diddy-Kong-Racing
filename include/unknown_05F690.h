f87dcd5e36c4480a17a848f342c5965c7f722f74
Antonio Castelli <aatcastelli@gmail.com>
2021-06-02 19:35:49 -0700

    Moved symbols in unknown_05F690 to header file.

---
+++ include/unknown_05F690.h
@@ -0,0 +1,11 @@
+#ifndef _UNKNOWN_05F690_H_
+#define _UNKNOWN_05F690_H_
+
+#include "types.h"
+
+extern s16 D_800DCE60[16];
+extern s16 D_800DCE80[16];
+extern s32 D_800DCEA0;
+extern s32 D_800DCEA4;
+
+#endif
