721887a4d99c2db738097af67e7604fd0c7bc9c3
David Benepe <benepe96@gmail.com>
2021-06-05 15:00:16 -0500

    Moved rodata from C to GLOBAL_ASM in unknown_05F690

--- src/unknown_05F690.c
+++ src/unknown_05F690.c
@@ -15,22 +15,4 @@ s16 D_800DCE60[14] = {
 
 /*******************************/
 
-/************ .rodata ************/
-
-const DoubleLiteral D_800E6AB0 = { 0.3 };
-const DoubleLiteral D_800E6AB8 = { -0.3 };
-const DoubleLiteral D_800E6AC0 = { 0.4 };
-const DoubleLiteral D_800E6AC8 = { 0.45 };
-
-// Jump table in func_8005EA90
-const u32 D_800E6AD0[] = {
-    0x8005EE9C, 0x8005EEC0, 0x8005EEF8, 0x8005EF14,
-    0x8005EF6C, 0x8005EF98
-};
-
-const DoubleLiteral D_800E6AE8 = { -0.1 };
-const DoubleLiteral D_800E6AF0 = { 0.1 };
-
-/*********************************/
-
 GLOBAL_ASM("asm/non_matchings/unknown_05F690/func_8005EA90.s")
