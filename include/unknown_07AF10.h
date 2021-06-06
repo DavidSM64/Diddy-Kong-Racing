07d2eacb93490774c4e828fb66d691c541007bfa
Antonio Castelli <aatcastelli@gmail.com>
2021-06-06 13:02:25 -0700

    Moved symbols in unknown_07AF10 to header file.

---
+++ include/unknown_07AF10.h
@@ -0,0 +1,20 @@
+#ifndef _UNKNOWN_07AF10_H_
+#define _UNKNOWN_07AF10_H_
+
+#include "types.h"
+
+extern s32 D_800DE770;
+extern s32 D_800DE774;
+extern s8  D_800DE778;
+
+/* Size: 0x08 bytes */
+typedef struct unk800DE77C {
+    /* 0x00 */ s32 unk0;
+    /* 0x04 */ s32 unk4;
+} unk800DE77C;
+
+extern unk800DE77C D_800DE77C[8];
+
+extern s32 D_800DE7BC;
+
+#endif
