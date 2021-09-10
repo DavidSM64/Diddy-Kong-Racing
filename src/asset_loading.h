2e6527c4c78a7b8bb90aea1f6e8a41589cce7020
Ryan Myers <ryan.p.myers@gmail.com>
2021-09-10 09:51:15 -0400

    Added function signatures to asset_loading.h, and cleaned up one from memory

--- src/asset_loading.h
+++ src/asset_loading.h
@@ -1,4 +1,14 @@
 #ifndef _UNKNOWN_0777A0_H_
 #define _UNKNOWN_0777A0_H_
 
+#include "types.h"
+
+void func_80076BA0(void);
+s32 *load_asset_section_from_rom(u32 assetIndex);
+s32 load_asset_section_from_rom_to_address(u32 assetIndex, s32 *address);
+s32 load_asset_to_address(u32 assetIndex, s32 *address, s32 assetOffset, s32 size);
+u8 *get_rom_offset_of_asset(u32 assetIndex, s32 assetOffset);
+s32 get_size_of_asset_section(u32 assetIndex);
+void dmacopy(u32 romOffset, u32 ramAddress, s32 numBytes);
+
 #endif
