1be1d00092c6ed6fa7e994b8d04fd1de412c07a5
Ryan Myers <ryan.p.myers@gmail.com>
2021-09-12 12:12:01 -0400

    Create main.h

---
+++ src/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "types.h"
+#include "macros.h"
+#include "libultra_internal.h"
+
+void thread3_main(s32 arg0); //unknown_06B2B0.c
+
+void main(void);
+
+void func_80065D98(s32 arg0); //Non Matching
+//void func_80065E30(void); //Non Matching
+
+#endif
