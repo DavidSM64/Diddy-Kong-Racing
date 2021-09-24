65c2cc17df816cf1939f51c0546db2159720b9c2
Ryan Myers <ryan.p.myers@gmail.com>
2021-09-24 13:21:28 -0400

    Matched osAiSetFrequency

--- lib/src/os/osAiSetFrequency.c
+++ lib/src/os/osAiSetFrequency.c
@@ -3,5 +3,28 @@
 
 #include "types.h"
 #include "macros.h"
+#include "libultra_internal.h"
 
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0C91A0/osAiSetFrequency.s")
+extern s32 osViClock;
+s32 osAiSetFrequency(u32 frequency) {
+    register u32 dacRate;
+    register u8 bitRate;
+    register float f;
+    
+	f = osViClock / (float)frequency + .5f;
+    dacRate = f;
+    
+	if (dacRate < AI_MIN_DAC_RATE)
+        return -1;
+    
+	bitRate = (dacRate / 66);
+    
+	if (bitRate > AI_MAX_BIT_RATE)
+        bitRate = AI_MAX_BIT_RATE;
+
+    IO_WRITE(AI_DACRATE_REG, dacRate - 1);
+    IO_WRITE(AI_BITRATE_REG, bitRate - 1);
+    IO_WRITE(AI_CONTROL_REG, AI_CONTROL_DMA_ON);
+    
+	return osViClock / (s32)dacRate;
+}
