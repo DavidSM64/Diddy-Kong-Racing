5191fe0c6b672e3291af85c758b5df83d60e8d40
Ryan Myers <ryan.p.myers@gmail.com>
2021-09-27 15:15:28 -0400

    Match osEepromProbe

--- lib/src/os/osEepromProbe.c
+++ lib/src/os/osEepromProbe.c
@@ -1,7 +1,21 @@
 /* The comment below is needed for this file to be picked up by generate_ld */
 /* RAM_POS: 0x800CE210 */
 
-#include "types.h"
-#include "macros.h"
+#include "libultra_internal.h"
 
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0CEE10/osEepromProbe.s")
+s32 __osEepStatus(OSMesgQueue *mq, OSContStatus *data);
+
+s32 osEepromProbe(OSMesgQueue *mq) {
+    s32 ret = 0;
+    OSContStatus sdata;
+
+    __osSiGetAccess();
+    ret = __osEepStatus(mq, &sdata);
+    if (ret == 0 && (sdata.type & CONT_EEPROM) != 0) {
+        ret = 1;
+    } else {
+        ret = 0;
+    }
+    __osSiRelAccess();
+    return ret;
+}
