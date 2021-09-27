8211a38f8c4f8bcbacd1b5b85ee92399ebbc9c5f
Ryan Myers <ryan.p.myers@gmail.com>
2021-09-27 15:26:41 -0400

    Match osViSetMode

--- lib/src/os/osViSetMode.c
+++ lib/src/os/osViSetMode.c
@@ -1,7 +1,14 @@
 /* The comment below is needed for this file to be picked up by generate_ld */
 /* RAM_POS: 0x800D1CA0 */
 
-#include "types.h"
-#include "macros.h"
+#include "libultra_internal.h"
+#include "viint.h"
 
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0D28A0/osViSetMode.s")
+void osViSetMode(OSViMode *modep) {
+    register u32 saveMask;
+    saveMask = __osDisableInt();
+    __osViNext->modep = modep;
+    __osViNext->state = VI_STATE_01;
+    __osViNext->control = __osViNext->modep->comRegs.ctrl;
+    __osRestoreInt(saveMask);
+}
