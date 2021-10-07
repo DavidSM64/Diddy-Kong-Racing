889fd2187d99526714c5ed645ddf20f5384fb912
Ryan Myers <ryan.p.myers@gmail.com>
2021-10-06 21:25:05 -0400

    Match pfsreadwritefile

--- lib/src/os/unknown_0CF330.c
+++
@@ -1,8 +0,0 @@
-/* The comment below is needed for this file to be picked up by generate_ld */
-/* RAM_POS: 0x800CEEE0 */
-
-#include "types.h"
-#include "macros.h"
-
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0CF330/__osPfsGetNextPage.s")
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0CF330/osPfsReadWriteFile.s")
