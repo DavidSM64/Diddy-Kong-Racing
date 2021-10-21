9200805699b387f7620aafe9c6e41d401135d6f1
Ryan Myers <ryan.p.myers@gmail.com>
2021-10-21 08:52:04 -0400

    __osSiDeviceBusy from libreultra

--- lib/src/os/unknown_0D5EC0.c
+++ lib/src/os/unknown_0D5EC0.c
@@ -1,11 +1,10 @@
 /* The comment below is needed for this file to be picked up by generate_ld */
-/* RAM_POS: 0x800D52C0 */
+/* RAM_POS: 0x800D52EC */
 
 #include "macros.h"
 #include "libultra_internal.h"
 #include "viint.h"
 
-GLOBAL_ASM("lib/asm/non_matchings/unknown_0D3020/__osSiDeviceBusy.s")
 GLOBAL_ASM("lib/asm/non_matchings/unknown_0D3020/__osSumcalc.s")
 GLOBAL_ASM("lib/asm/non_matchings/unknown_0D3020/__osIdCheckSum.s")
 GLOBAL_ASM("lib/asm/non_matchings/unknown_0D3020/__osRepairPackId.s")
