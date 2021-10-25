a3f1ed5821f85573cef57a15f3a29afc6680caad
Ryan Myers <ryan.p.myers@gmail.com>
2021-10-24 22:09:53 -0400

    Match func_800AD40C

--- src/unknown_008C40.h
+++ src/unknown_008C40.h
@@ -5,6 +5,7 @@
 
 void func_80008168(void);
 
+void func_800096F8(s32); // Non Matching
 void func_80009558(u16, f32 x, f32 y, f32 z, s32, s32 *); // Non Matching
 void func_80009968(f32, f32, f32, u8, u8, s32); // Non Matching
 void func_800096D8(s32, f32, f32, f32); // Non Matching
