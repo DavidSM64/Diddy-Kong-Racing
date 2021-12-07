a70af4d7af095797d2c814efd7d96bc2a52f0831
Fazana <52551480+FazanaJ@users.noreply.github.com>
2021-12-07 01:02:36 +0000

    deleted linker failsafe

--- tools/python/generate_ld.py
+++ tools/python/generate_ld.py
@@ -228,6 +228,11 @@ class GenerateLD:
         files.sort(key = lambda x: (x[2], x[3])) # Sort tuples by RAM address and prioritize src files first.
         return files
 
+if os.path.exists('./dkr.ld'):
+    print("")
+else:
+    os.remove('./assets/' + VERSION + '/md5.txt')
+
 if os.path.exists('./assets/' + VERSION + '/md5.txt'):
     with open('./assets/' + VERSION + '/md5.txt', "rt") as a_file:
         with open('./assets/' + VERSION + '/assets.json',"rb") as f:
