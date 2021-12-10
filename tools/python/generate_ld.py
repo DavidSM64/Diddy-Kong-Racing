6e1ac97c3af8e4bcaf48a77770407c469335638e
Fazana <52551480+FazanaJ@users.noreply.github.com>
2021-12-10 09:40:34 +0000

    Update generate_ld.py

--- tools/python/generate_ld.py
+++ tools/python/generate_ld.py
@@ -228,9 +228,7 @@ class GenerateLD:
         files.sort(key = lambda x: (x[2], x[3])) # Sort tuples by RAM address and prioritize src files first.
         return files
 
-if os.path.exists('./dkr.ld'):
-    print("")
-else:
+if not os.path.exists('./dkr.ld') and os.path.exists('./assets/' + VERSION + '/md5.txt'):
     os.remove('./assets/' + VERSION + '/md5.txt')
 
 if os.path.exists('./assets/' + VERSION + '/md5.txt'):
