1a669f265a4009eab67d17aeb9ed849e22d3e594
Ryan Myers <ryan.p.myers@gmail.com>
2022-02-24 12:57:42 -0500

    Fix warnings in asset_loading.c

--- src/unknown_008C40.h
+++ src/unknown_008C40.h
@@ -9,6 +9,7 @@ void func_800096F8(s32);
 void func_80009558(u16, f32 x, f32 y, f32 z, s32, s32 *); // Non Matching
 void func_80009968(f32, f32, f32, u8, u8, s32); // Non Matching
 void func_800096D8(s32, f32, f32, f32); // Non Matching
+void func_8000A2E8(s32); // Non Matching
 // Yeah this one seems excessive, and it's Non Matching
 void func_800098A4(s32, s32, f32, f32, f32, s32, s32, s32, s32, s32, s32, s32, s32);
 
