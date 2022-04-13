a3a4d42e7c3927c075b402a6110e88df2eb81393
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-04-13 12:53:29 +0100

    Labelled trig tables

--- asm/unknown_070110/func_8007066C.s
+++ asm/unknown_070110/func_8007066C.s
@@ -33,8 +33,8 @@ glabel func_8007066C
 /* 0712D0 800706D0 000442F8 */   dsll  $t0, $a0, 0xb
 /* 0712D4 800706D4 000542F8 */  dsll  $t0, $a1, 0xb
 /* 0712D8 800706D8 0104001F */  ddivu $zero, $t0, $a0
-/* 0712DC 800706DC 3C09800E */  lui   $t1, %hi(D_800DDC3E) # $t1, 0x800e
-/* 0712E0 800706E0 2529DC3E */  addiu $t1, %lo(D_800DDC3E) # addiu $t1, $t1, -0x23c2
+/* 0712DC 800706DC 3C09800E */  lui   $t1, %hi(gArcTanTable) # $t1, 0x800e
+/* 0712E0 800706E0 2529DC3E */  addiu $t1, %lo(gArcTanTable) # addiu $t1, $t1, -0x23c2
 /* 0712E4 800706E4 24424000 */  addiu $v0, $v0, 0x4000
 /* 0712E8 800706E8 14800002 */  bnez  $a0, .L800706F4
 /* 0712EC 800706EC 00000000 */   nop   
@@ -51,8 +51,8 @@ glabel func_8007066C
 /* 071314 80070714 000442F8 */  dsll  $t0, $a0, 0xb
 .L80070718:
 /* 071318 80070718 0105001F */  ddivu $zero, $t0, $a1
-/* 07131C 8007071C 3C09800E */  lui   $t1, %hi(D_800DDC3E) # $t1, 0x800e
-/* 071320 80070720 2529DC3E */  addiu $t1, %lo(D_800DDC3E) # addiu $t1, $t1, -0x23c2
+/* 07131C 8007071C 3C09800E */  lui   $t1, %hi(gArcTanTable) # $t1, 0x800e
+/* 071320 80070720 2529DC3E */  addiu $t1, %lo(gArcTanTable) # addiu $t1, $t1, -0x23c2
 /* 071324 80070724 14A00002 */  bnez  $a1, .L80070730
 /* 071328 80070728 00000000 */   nop   
 /* 07132C 8007072C 0007000D */  break 7
--- asm/unknown_070110/func_80070890.s
+++ asm/unknown_070110/func_80070890.s
@@ -6,9 +6,9 @@ glabel func_80070890
 /* 07149C 8007089C 38847FFF */  xori  $a0, $a0, 0x7fff
 /* 0714A0 800708A0 000450C2 */  srl   $t2, $a0, 3
 .L800708A4:
-/* 0714A4 800708A4 3C02800E */  lui   $v0, %hi(D_800DD43C) # $v0, 0x800e
+/* 0714A4 800708A4 3C02800E */  lui   $v0, %hi(gSineTable) # $v0, 0x800e
 /* 0714A8 800708A8 314A07FE */  andi  $t2, $t2, 0x7fe
-/* 0714AC 800708AC 2442D43C */  addiu $v0, %lo(D_800DD43C) # addiu $v0, $v0, -0x2bc4
+/* 0714AC 800708AC 2442D43C */  addiu $v0, %lo(gSineTable) # addiu $v0, $v0, -0x2bc4
 /* 0714B0 800708B0 004A1021 */  addu  $v0, $v0, $t2
 /* 0714B4 800708B4 94420000 */  lhu   $v0, ($v0)
 /* 0714B8 800708B8 00042400 */  sll   $a0, $a0, 0x10
