8266c0b2bba22fb52f1e459b75b0949f23c91afc
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-05-15 16:05:10 +0100

    Update math_util.h

--- include/math_util.h
+++ include/math_util.h
@@ -4,8 +4,10 @@
 #include "types.h"
 #include "structs.h"
 
-extern s16 arctan2_f(f32 y, f32 x);
-extern f32 sine_s(s16 angle);
-extern f32 cosine_s(s16 angle);
+s16 arctan2_f(f32 y, f32 x);
+f32 sine_s(s16 angle);
+f32 cosine_s(s16 angle);
+void func_80031130(s32, f32*, f32*, s32);
+s32 func_80031600(f32*, f32*, s32*, s8*, s32, s32*);
 
 #endif // MATH_UTIL_H
