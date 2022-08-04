46d32d65f51851b06b0218b70d93a341e70fea01
Ryan Myers <ryan.p.myers@gmail.com>
2022-08-04 09:45:36 -0400

    Match alFxPull

--- lib/src/mips1/al/reverb.c
+++ lib/src/mips1/al/reverb.c
@@ -5,17 +5,140 @@
 #include "macros.h"
 #include "libultra_internal.h"
 #include "audio_internal.h"
+#include "initfx.h"
 
-/************ .data ************/
+// TODO: these come from headers
+#ident "$Revision: 1.49 $"
+#ident "$Revision: 1.17 $"
 
-s8 D_800DCEB0 = 1;
-s32 D_800DCEB4 = 0x10; // Currently unknown, might be a different type.
-s32 D_800DCEB8 = 0x10; // Currently unknown, might be a different type.
-s32 D_800DCEBC = 0x20; // Currently unknown, might be a different type.
+#define RANGE 2.0
+extern ALGlobals *alGlobals;
 
-/*******************************/
+#ifdef AUD_PROFILE
+extern u32 cnt_index, reverb_num, reverb_cnt, reverb_max, reverb_min, lastCnt[];
+extern u32 load_num, load_cnt, load_max, load_min, save_num, save_cnt, save_max, save_min;
+#endif
 
-GLOBAL_ASM("lib/asm/non_matchings/reverb/alFxPull.s")
+/*
+ * macros
+ */
+#define SWAP(in, out)	\
+{			\
+     s16 t = out;	\
+     out = in;		\
+     in = t;		\
+}
+
+
+
+Acmd *_loadOutputBuffer(ALFx *r, ALDelay *d, s32 buff, s32 incount, Acmd *p);
+Acmd *_loadBuffer(ALFx *r, s16 *curr_ptr, s32 buff, s32 count, Acmd *p);
+Acmd *_saveBuffer(ALFx *r, s16 *curr_ptr, s32 buff, s32 count, Acmd *p);
+Acmd *_filterBuffer(ALLowPass *lp, s32 buff, s32 count, Acmd *p);
+f32  _doModFunc(ALDelay *d, s32 count);
+
+u8 D_800DCEB0 = 1;
+static s32 L_INC[] = { L0_INC, L1_INC, L2_INC };
+
+/***********************************************************************
+ * Reverb filter public interfaces
+ ***********************************************************************/
+Acmd *alFxPull(void *filter, s16 *outp, s32 outCount, s32 sampleOffset,
+                     Acmd *p) 
+{
+    Acmd        *ptr = p;
+    ALFx	*r = (ALFx *)filter;
+    ALFilter    *source = r->filter.source;
+    s16		i, buff1, buff2, input, output;
+    s16		*in_ptr, *out_ptr, *prev_out_ptr = 0;
+    ALDelay	*d;
+
+#ifdef AUD_PROFILE
+    lastCnt[++cnt_index] = osGetCount();
+#endif
+    
+#ifdef _DEBUG
+    assert(source);
+#endif
+
+    /*
+     * pull channels going into this effect first
+     */
+    ptr = (*source->handler)(source, outp, outCount, sampleOffset, p);
+
+    //Added by Rare Devs
+    if (D_800DCEB0 == 0) {
+        return ptr;
+    }
+
+    input  = AL_AUX_L_OUT;
+    output = AL_AUX_R_OUT;
+    buff1  = AL_TEMP_0;
+    buff2  = AL_TEMP_1;
+    
+    aSetBuffer(ptr++, 0, 0, 0, outCount<<1);  /* set the buffer size */
+    aMix(ptr++, 0, 0xda83, AL_AUX_L_OUT, input); /* .707L = L - .293L */
+    aMix(ptr++, 0, 0x5a82, AL_AUX_R_OUT, input); /* mix the AuxL and AuxR into the AuxL */
+    /* and write the mixed value to the delay line at r->input */
+    ptr = _saveBuffer(r, r->input, input, outCount, ptr);
+
+    aClearBuffer(ptr++, output, outCount<<1); /* clear the AL_AUX_R_OUT */
+
+    for (i = 0; i < r->section_count; i++) {
+	d  = &r->delay[i];  /* get the ALDelay structure */
+	in_ptr  = &r->input[-d->input];
+	out_ptr = &r->input[-d->output];
+	
+	if (in_ptr == prev_out_ptr) {
+	    SWAP(buff1, buff2);
+	} else {  /* load data at in_ptr into buff1 */
+	    ptr = _loadBuffer(r, in_ptr, buff1, outCount, ptr);
+	}
+	ptr = _loadOutputBuffer(r, d, buff2, outCount, ptr);
+
+	if (d->ffcoef) {
+	    aMix(ptr++, 0, (u16)d->ffcoef, buff1, buff2);
+	    if (!d->rs && !d->lp) {
+		ptr = _saveBuffer(r, out_ptr, buff2, outCount, ptr);
+	    }
+	}
+	
+	if (d->fbcoef) {
+	    aMix(ptr++, 0, (u16)d->fbcoef, buff2, buff1);
+	    ptr = _saveBuffer(r, in_ptr, buff1, outCount, ptr);
+	}
+
+	if (d->lp)
+	    ptr = _filterBuffer(d->lp, buff2, outCount, ptr);
+
+	if (!d->rs)
+	    ptr = _saveBuffer(r, out_ptr, buff2, outCount, ptr);
+	
+	if (d->gain)
+	    aMix(ptr++, 0, (u16)d->gain, buff2, output);
+	
+	prev_out_ptr = &r->input[d->output];
+    }
+
+    /*
+     * bump the master delay line input pointer
+     * modulo the length
+     */
+    r->input += outCount;
+    if (r->input > &r->base[r->length])
+	r->input -= r->length;
+
+    /*
+     * output already in AL_AUX_R_OUT
+     *      just copy to AL_AUX_L_OUT
+     */
+    aDMEMMove(ptr++, output, AL_AUX_L_OUT, outCount<<1);
+
+#ifdef AUD_PROFILE
+    PROFILE_AUD(reverb_num, reverb_cnt, reverb_max, reverb_min);
+#endif
+    return ptr;
+}
 
 s32 alFxParam(void *filter, s32 paramID, void *param) {
     if (paramID == 1) {
