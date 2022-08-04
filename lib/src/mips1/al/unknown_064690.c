22955b4a0689168679a5f37880e26e9f8cb7a76e
Ryan Myers <ryan.p.myers@gmail.com>
2022-08-04 10:58:57 -0400

    Clean up func_80063A90

--- lib/src/mips1/al/unknown_064690.c
+++ lib/src/mips1/al/unknown_064690.c
@@ -3,26 +3,15 @@
 
 #include "types.h"
 #include "macros.h"
+#include "audio_internal.h"
 
-/* Size: 0x0C bytes */
-typedef struct unk80063BA0 {
-    s16 unk0;
-    s16 unk2;
-    s32 unk4;
-    u8 unk8;
-    u8 unk9;
-    u8 unkA;
-} unk80063BA0;
+void func_80063A90(ALSeqPlayer *seqp, u8 chan) {
+    ALEvent evt;
 
-void func_80063A90(u32 arg0, u8 arg1) {
-    s32 unusedVariable;
-    unk80063BA0 unknownStruct;
-    unknownStruct.unk0 = 2;
-    unknownStruct.unk4 = 0;
-    unknownStruct.unk8 = 0xB0;
-    unknownStruct.unk9 = 0x5F;
-    unknownStruct.unkA = arg1;
-    alEvtqPostEvent(arg0 + 0x48, &unknownStruct, 0);
+    evt.type = AL_SEQP_MIDI_EVT;
+    evt.msg.midi.ticks  = 0;
+    evt.msg.midi.status = AL_MIDI_ControlChange;
+    evt.msg.midi.byte1  = 0x5F;
+    evt.msg.midi.byte2  = chan;
+    alEvtqPostEvent(&seqp->evtq, &evt, 0);
 }
-
-
