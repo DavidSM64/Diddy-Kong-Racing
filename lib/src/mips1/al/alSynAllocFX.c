00b9ba3f8336d8553bc8be8342eebf6104bd24ea
Ryan Myers <ryan.p.myers@gmail.com>
2022-08-07 13:21:37 -0400

    Fix codebreaking formatting error ;)

--- lib/src/mips1/al/alSynAllocFX.c
+++ lib/src/mips1/al/alSynAllocFX.c
@@ -10,7 +10,7 @@ ALFxRef *alSynAllocFX(ALSynth *s, s16 bus, ALSynConfig *c, ALHeap *hp)
     alFxNew(&s->auxBus[bus].fx[0], c, bus, hp);
     alFxParam(&s->auxBus[bus].fx[0], AL_FILTER_SET_SOURCE,
                   &s->auxBus[bus]);
-    alMainBusParam(s->mainBus, AL_FILTER_ADD_SOURCE,&s->auxBus[bus].fx[0]);
+    alMainBusParam(s->mainBus, AL_FILTER_ADD_SOURCE, &s->auxBus[bus].fx[0]);
 
     return (ALFxRef)(&s->auxBus[bus].fx[0]);
 }
