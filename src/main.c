2fed701b78c85dcbdb280bf340fd65bfd4dee611
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-09-28 10:29:29 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,8 +14,46 @@ u64 *gThread3StackPointer; // stack pointer for thread 3
 OSThread gThread1; // OSThread for thread 1
 OSThread gThread3; // OSThread for thread 3
 u64 D_8011FAC0[512]; //Thread1 stack?
+u8 gPlatformSet = FALSE;
+u8 gPlatform;
 
 /******************************/
+
+u32 get_clockspeed(void);
+void skGetId(u32 *arg);
+
+void get_platform(void)
+{
+    /*u32 notiQue;
+    if (get_clockspeed() == 0 || (u32)IO_READ(DPC_PIPEBUSY_REG) + (u32)IO_READ(DPC_TMEM_REG) + (u32)IO_READ(DPC_BUFBUSY_REG))
+    {
+        gPlatform &= ~EMULATOR;
+        gPlatform |= CONSOLE;
+    }
+    else
+    {
+        gPlatform &= ~CONSOLE;
+        gPlatform |= EMULATOR;
+    }*/
+
+    /*if (IS_VC())
+    {
+        gPlatform &= ~CONSOLE;
+        gPlatform |= EMULATOR;
+        gPlatform |= VC;
+    }
+    else
+    {*/
+        //gPlatform &= ~VC;
+    //}
+
+    /*skGetId(&notiQue);
+    if (!notiQue)
+    {
+        gPlatform |= IQUE;
+        gPlatform &= EMULATOR;
+    }*/
+}
 
 void main(void) {
     osInitialize();
