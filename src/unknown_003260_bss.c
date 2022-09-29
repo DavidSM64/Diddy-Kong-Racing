f396463e95cf67d032a683dd68827718c1e417b5
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-09-29 16:06:38 +0100

    Minor misc labelling (#246)
    * Squashed commit of the following:
    
    commit 3ab471611006d05553f2fea56ace0a92397344cf
    Author: Fazana <52551480+FazanaJ@users.noreply.github.com>
    Date:   Mon May 23 23:11:46 2022 +0100
    
        Mostly misc labelling, but also weather too (#232)
    
        * bit more labelling
    
        * identify some weather stuff
    
        * Update sound_ids.h
    
        * yum
    
        * more random identification
    
        * last weather stuff for now
    
        * Update README.md
    
        * Update README.md
    
        * Fix non equivalent
    
    * I found the 60fps button
    
    * match func_80052B64
    
    * match racer_attack_handler
    
    * match func_80048C7C
    
    * buncha function declaring
    
    * e
    
    * tweak
    
    * match func_8004DE38
    
    oh man oh man
    
    * Small bit of labelling
    
    * Merge remote-tracking branch 'upstream/master' into the-many-against-the-few
    
    * Update README.md
    
    * Fix monumentally poor bout of misinformation in the readme
    
    we wouldn't want to mislead the readers, would we?
    
    * github please
    
    * Update README.md
    
    * requested changes
    
    kinda funny because I knew this struct existed, and knew it was used for this, but never made the connection lol
    
    * Update objects.c
    
    * match racer_enter_door
    
    A nice easy one after the pure hellscape of the last few days :)
    
    * names
    
    * renames
    
    * match func_8005250C
    
    * heehee
    
    * Update racer.c
    
    * identify some misc asset calls
    
    * identify some surface types
    
    * match func_8005492C
    
    * Delete func_8005492C.s
    
    * match func_80052D7C
    
    * move some enums
    
    * label all existing misc asset calls
    
    granted I know what almost none of them are lmao, buut will make things slightly easier in the long run
    
    * Update README.md
    
    * requested changes
    
    pain
    
    * Update README.md
    
    * just a bit of labelling

--- src/unknown_003260_bss.c
+++ src/unknown_003260_bss.c
@@ -4,12 +4,12 @@
 #include "unknown_003260.h"
 // This file is needed, since there is a bss reordering issue with D_80119BD0 and gAlSndPlayer
 
-OSSched *D_80115F90;
+OSSched *gAudioSched;
 ALHeap *D_80115F94;
 s32 D_80115F98[2];
 s32 D_80115FA0[3];
 OSThread audioThread;
-OSMesgQueue OSMesgQueue_80116160;
+OSMesgQueue gAudioMesgQueue;
 OSMesg D_80116178[8];
 OSMesgQueue D_80116198;
 OSMesg D_801161B0[8];
