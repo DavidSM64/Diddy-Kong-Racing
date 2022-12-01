0beabf75cc92037be2b1d17d83049b1db3de91ce
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-12-01 19:19:58 +0000

    Update src/math_util.c
    Co-authored-by: David Benepe <benepe96@gmail.com>

--- src/math_util.c
+++ src/math_util.c
@@ -129,7 +129,8 @@ void s16_matrix_rotate(s16 *arg0[4][4], s16 arg1[4][4]) {
     temp_t6 = (s32) ((*arg0[0][0] * arg1[0][0]) + (*arg0[0][1] * arg1[0][1]) + (*arg0[0][2] * *arg0[0][2])) >> 12;
     *arg0[1][0] = (s16) (((s32) (temp_t6 * arg1[0][0]) >> 13) - *arg0[0][0]);
     *arg0[1][1] = (s16) (((s32) (temp_t6 * arg1[0][1]) >> 13) - *arg0[0][1]);
-    *arg0[1][2] = (s16) (((s32) (temp_t6 * arg1[0][2]) >> 13) - *arg0[0][0]); // Did they mean to do `- arg0->unk4` here?
+    *arg0[1][2] = (s16) (((s32) (temp_t6 * arg1[0][2]) >> 13) - *arg0[0][0]); // Did they mean to do `- *arg0[0][2]` here?
+
 }
 #else
 GLOBAL_ASM("asm/math_util/s16_matrix_rotate.s")
