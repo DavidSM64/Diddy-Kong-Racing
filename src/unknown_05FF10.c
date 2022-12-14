748b3666964693eb276915e39093c8f598157cdc
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-12-14 20:34:29 +0000

    Update unknown_05FF10.c

--- src/unknown_05FF10.c
+++ src/unknown_05FF10.c
@@ -151,7 +151,7 @@ void func_8005F310(s32 updateRate, f32 updateRateF, Object *obj, Object_Racer *r
     }
     tempObj = get_racer_object(0);
     obj64 = tempObj->unk64;
-    if ((obj == tempobj->interactObj->obj) && (tempObj->interactObj->unk14 & 8) && (obj->segment.unk3B == 1)) {
+    if ((obj == tempObj->interactObj->obj) && (tempObj->interactObj->unk14 & 8) && (obj->segment.unk3B == 1)) {
         obj64->racer.attackType = ATTACK_SQUISHED;
     }
     if ((obj64->racer.raceFinished != FALSE) && (D_8011D610 == 0)) {
