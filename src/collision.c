59303e4fd4124bcca03372aa3b88596468d2e8c2
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-12-26 22:51:05 +0000

    Update collision.c

--- src/collision.c
+++ src/collision.c
@@ -14,61 +14,70 @@
 GLOBAL_ASM("asm/collision/func_80031130.s")
 // Might be segment related.
 #ifdef NON_EQUIVALENT
-s32 func_800314DC(LevelModelSegmentBoundingBox* segment, s32 x1, s32 z1, s32 x2, s32 z2) {
-    s32 segDiff;
-    s32 segMin;
-    s32 ret = 0;
+s32 func_800314DC(LevelModelSegmentBoundingBox* a0, s32 a1, s32 a2, s32 a3, s32 t5) {
+    s32 v0 = 0;
     s32 v1;
-    s32 x1temp;
-    s32 z1temp;
-    
-    if (segment != NULL) {
-        if (x2 < segment->x1) {
-            x2 = segment->x1;
-        }
-        if (x1 < segment->x1) {
-            x1 = segment->x1;
-        }
-        if (z2 < segment->z1) {
-            z2 = segment->z1;
-        }
-        if (z1 < segment->z1) {
-            z1 = segment->z1;
-        }
-        if (segment->x2 < x2) {
-            x2 = segment->x2;
-        }
-        if (segment->x2 < x1) {
-            x1 = segment->x2;
-        }
-        if (segment->z2 < z2) {
-            z2 = segment->z2;
-        }
-        if (segment->z2 < z1) {
-            z1 = segment->z2;
-        }
-        segDiff = ((segment->x2 - segment->x1) >> 3) + 1;
-        segMin = segDiff + segment->x1;
-        x1temp = segment->x1;
-        for (v1 = 1; v1 < (1 << 8); v1 <<= 1) {
-            if (segMin >= x1 && x2 >= x1temp) {
-                ret |= v1;
-            }
-            segMin += segDiff;
-            x1temp += segDiff;
-        }
-        segDiff = ((segment->z2 - segment->z1) >> 3) + 1;
-        segMin = segDiff + segment->z1;
-        z1temp = segment->z1;
-        for (; v1 < (1 << 16); v1 <<= 1) {
-            if (segMin >= z1 && z2 >= z1temp) {
-                ret |= v1;
-            }
-            segMin += segDiff;
-            z1temp += segDiff;
-        }
+    s32 t0, t1, t2, t3, t4;
+
+    if (a0 == NULL)
+        return v0;
+
+    t0 = a0->x1;
+    t1 = a0->z1;
+    t2 = a0->x2;
+    t3 = a0->z2;
+
+    if (a3 < t0)
+        a3 = t0;
+
+    if (a1 < t0)
+        a1 = t0;
+
+    if (t5 < t1)
+        t5 = t1;
+
+    if (a2 < t1)
+        a2 = t1;
+
+    if (t2 < a3)
+        a3 = t2;
+
+    if (t2 < a1)
+        a1 = t2;
+
+    if (t3 < t5)
+        t5 = t3;
+
+    if (t3 < a2)
+        a2 = t3;
+
+    v1 = 1;
+
+    t2 = ((t2 - t0) >> 3) + 1;
+    t4 = t2 + t0;
+
+    while (v1 < (1 << 8)) {
+        if (t4 >= a1 && a3 >= t0)
+            v0 |= v1;
+
+        v1 <<= 1;
+        t4 += t2;
+        t0 += t2;
     }
-    return ret;
+
+    t2 = ((t3 - t1) >> 3) + 1;
+    t4 = t2 + t1;
+
+    while (v1 < (1 << 16)) {
+        if (t4 >= a2 && t5 >= t1)
+            v0 |= v1;
+
+        v1 <<= 1;
+        t4 += t2;
+        t1 += t2;
+    }
+
+    return v0;
 }
 #else
 GLOBAL_ASM("asm/collision/func_800314DC.s")
