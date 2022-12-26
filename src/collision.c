b9d15e59f963d47ed6b4a29def3cbb2747698357
Fazana <52551480+FazanaJ@users.noreply.github.com>
2022-12-26 14:37:35 +0000

    one more

--- src/collision.c
+++ src/collision.c
@@ -10,6 +10,9 @@
 
 // All handwritten assembly, below.
 
+// Collision for a different segment
 GLOBAL_ASM("asm/collision/func_80031130.s")
+// Might be segment related.
 GLOBAL_ASM("asm/collision/func_800314DC.s")
+// Player collision for the current segment
 GLOBAL_ASM("asm/collision/func_80031600.s")
--- src/collision.h
+++ src/collision.h
@@ -5,6 +5,7 @@
 #include "structs.h"
 
 void func_80031130(s32, f32 *, f32*, s32);
+void func_800314DC(LevelModelSegmentBoundingBox *arg0, s16 arg1, s16 arg2, s16 arg3, s32 arg4);
 s32 func_80031600(f32 *pos, f32 *pos2, f32*, s8* surface, s32, s32 *);
 
 #endif
