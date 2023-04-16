c41bbf9131f0608f97423e74fecb282673fd56e1
Fazana <52551480+FazanaJ@users.noreply.github.com>
2023-04-16 14:50:14 +0100

    Match 3 libultra funcs + some texture init labels (#383)
    * Fix warnings
    
    * Update src/thread0_epc.c
    
    Co-authored-by: Ryan Myers <ryan.p.myers@gmail.com>
    
    * identify some texture init stuff
    
    * match _Litob
    
    * match cosf
    
    * match sinf
    
    * formatting
    
    * Update math.h
    
    * Delete sinf.s
    
    * quick fix
    
    ---------
    
    Co-authored-by: Ryan Myers <ryan.p.myers@gmail.com>

--- include/libc/math.h
+++ include/libc/math.h
@@ -3,11 +3,8 @@
 
 #define M_PI 3.14159265358979323846
 
-float sinf(float); //official name: fsin
-double sin(double);
-float cosf(float);
-double cos(double);
-
-float sqrtf(float);
+f32 sinf(f32 x); //official name: fsin
+f32 cosf(f32 x);
+f32 sqrtf(f32 x);
 
 #endif
