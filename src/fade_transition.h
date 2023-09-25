6ad79efbd1a43f1fac690ddf321b4eeb30510024
Ryan Myers <ryan.p.myers@gmail.com>
2023-09-25 14:55:27 -0400

    Match func_800C1130

--- src/fade_transition.h
+++ src/fade_transition.h
@@ -59,9 +59,9 @@ void func_800C0834(s32 updateRate);
 void func_800C27A0(s32 updateRate);
 s32 transition_begin(FadeTransition *transition);
 void render_fade_circle(Gfx **dList, MatrixS **mats, Vertex **verts);
+void func_800C1130(s32 updateRate);
 
 void render_fade_waves(Gfx **dlist, UNUSED MatrixS **mats, UNUSED Vertex **verts); //Non Matching
-void func_800C1130(s32); //Non Matching
 void func_800C1EE8(s32); //Non Matching
 void func_800C0B00(FadeTransition *transition, s32, s32, s16*, s8*, s8*, s8*, s8*, s8*); //Non Matching
 void func_800C15D4(FadeTransition *transition); //Non Matching
