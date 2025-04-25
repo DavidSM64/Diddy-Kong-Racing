9577a9c22d7c39f134c7da80441bce12c5921b6c
Ryan Myers <ryan.p.myers@gmail.com>
2025-04-24 21:16:50 -0400

    Define the proper type for the stack_pointer function, and come up with a solution for GCC builds that should hopefully work.

--- src/get_stack_pointer.c
+++ src/get_stack_pointer.c
@@ -1,11 +1,16 @@
-#ifndef __sgi
-extern void *__$sp;
+#include "memory.h"
+
+#ifdef __sgi
+#define STACK_POINTER __$sp
+#else
+#define STACK_POINTER gStackPointer
+StackInfo *gStackPointer;
 #endif
 
 /**
  * Uses IDO specific -dollar compiler flag to get the current stack pointer.
  * Official Name: diCpuTraceCurrentStack
  */
-void *stack_pointer(void) {
-    return __$sp;
+StackInfo *stack_pointer(void) {
+    return (StackInfo *) STACK_POINTER;
 }
