7d56b447365a759de09829f4a8b63cc8308d2455
Ryan Myers <ryan.p.myers@gmail.com>
2025-05-07 12:05:33 -0400

    formatting

--- m2c_all.sh
+++ m2c_all.sh
@@ -7,14 +7,14 @@ mkdir -p m2cfiles
 find asm/nonmatchings -type f -name "*.s" | while read ASMFILE; do
     # Extract the filename without extension and path
     FILENAME=$(basename "$ASMFILE" .s)
-    
+
     echo "Processing $FILENAME from $ASMFILE"
-    
+
     # Create the output file with the required header
     echo '#include "../ctx.c"\n' > "m2cfiles/$FILENAME.c"
-    
+
     # Run the m2c command with the extracted filename
     .venv/bin/python3 tools/m2c/m2c.py --target mips-ido-c --pointer-style right --context ctx.c -f "$FILENAME" "$ASMFILE" >> "m2cfiles/$FILENAME.c"
-    
+
     echo "Created m2cfiles/$FILENAME.c"
 done
