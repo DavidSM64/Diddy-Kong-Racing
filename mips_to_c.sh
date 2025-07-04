d4974aed2f01808e1b864eb8b49eaec5d429ae24
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-04 09:25:15 -0400

    Update clang format to fix broken config settings

--- mips_to_c.sh
+++ mips_to_c.sh
@@ -4,5 +4,5 @@
 mkdir -p m2cfiles
 
 ASMFILE=$(find asm -type f -name "*$1.s")
-echo '#include "../ctx.c"\n' > m2cfiles/$1.c
-.venv/bin/python3 tools/m2c/m2c.py --target mips-ido-c --pointer-style right --context ctx.c -f $1 $ASMFILE >> m2cfiles/$1.c
+printf '#include "../ctx.c"\n' > m2cfiles/"$1".c
+.venv/bin/python3 tools/m2c/m2c.py --target mips-ido-c --pointer-style right --context ctx.c -f "$1" "$ASMFILE" >> m2cfiles/"$1".c
